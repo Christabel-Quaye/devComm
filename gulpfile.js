6f4a9d1169e644f91d3803bae63e2c7dd68647fb
Christabel Quaye <117347389+Christabel-Quaye@users.noreply.github.com>
2024-04-24 14:19:12 +0000

    styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("index.scss")
+  return src("sass/style.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(dest("css"));
 }
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["sass/style.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
